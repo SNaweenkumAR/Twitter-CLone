017e270b7d141bd051db986c2f6d551f3ca751cc
SNaweenkumAR <naween1924@gmail.com>
2024-12-15 15:04:28 +0530

    Set up database , write the auth controller and test positive

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express'
 const app = express();
-const PORT = 3500;
 
-app.get('/', (req,res) => {
-    res.send("Hello")
-})
+import dotenv from 'dotenv'
+dotenv.config();
+const PORT = process.env.PORT
+
+import authRoute from './routes/auth.route.js'
+import connectDB from './db/connectDB.js';
+
+app.use(express.json());
+
+app.use('/api/auth', authRoute)
 
 
 
 
 
 
-app.listen(PORT,() => {console.log(`Server Running at localhost:${PORT}`)})          
+
+app.listen(PORT,() => {console.log(`Server Running at localhost:${PORT}`)
+  connectDB()
+})          
