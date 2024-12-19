f8ed1402a788d25221af4143a0aa8d0fca833fa2
SNaweenkumAR <naween1924@gmail.com>
2024-12-19 15:01:23 +0530

    updated

--- backend/controller/post.controller.js
+++ backend/controller/post.controller.js
@@ -207,12 +207,7 @@ import mongoose from "mongoose";
                                 select:["-password", "-email" ,"-following","-followers","-link","-bio"]
                              })
 
-
-    if (!likedPosts.length) {
-        return res.status(200).json([]); // If no liked posts, return an empty array
-      }
-  
-      res.status(200).json(likedPosts);
+                             
         
     } catch (error) {
         console.error(`Error in Get Liked Posts Controller: ${error.message}`);
@@ -220,3 +215,35 @@ import mongoose from "mongoose";
     }
   }
   
+
+  export const getfollowing = async(req,res) =>{
+    try {
+        
+       const userId =  req.user._id;
+       const user = User.findById({_id : userId})
+
+       if(!user){
+        return res.status(404).json({error : "User Not Found"})
+       }
+
+       const following =  user.following
+
+       const feedPosts  =  await Post.find({user : { $in : following}})
+                             .sort({createAt : -1})
+                            .populate({
+                                path:"user",
+                                select:"-password"
+                            })
+                            .populate({
+                                path:"comments.user",
+                                select:["-password", "-email" ,"-following","-followers","-link","-bio"]
+                            })
+
+                            res.status(200).json(feedPosts)
+
+
+    } catch (error) {
+        console.error(`Error in Get Following Controller: ${error.message}`);
+        res.status(500).json({ error: "Internal server Error" });
+    }
+  }
