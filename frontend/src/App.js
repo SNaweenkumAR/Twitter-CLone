4c30b3400d05321d1508f37b923a7cbf192d9337
SNaweenkumAR <naween1924@gmail.com>
2024-12-20 10:14:22 +0530

    start the frontend with all setups includes tailwind too

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const App = () => {
+  return (
+    <div>
+    <h1 className='m-10 text-2xl text-green-400  bg-slate-400 rounded-md' >
+      Welcome to my channel
+    </h1>
+    </div>
+  )
+}
+
+export default App
