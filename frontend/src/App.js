5b5e65a46b6d2920d14148a8ac8a534b875ae914
SNaweenkumAR <naween1924@gmail.com>
2024-12-22 18:18:32 +0530

    finish sidebar still have works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import { Route , Routes } from 'react-router-dom'
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from './components/common/Sidebar';
 
 
  const App = () => {
   return (
     <div className='flex max-w-6xl mx-auto '>
+      <Sidebar/>
      <Routes>
       <Route path="/" element={<HomePage/>}></Route>
       <Route path="/login" element={<LoginPage/>}></Route>
